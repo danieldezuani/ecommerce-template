b4680ff9a2e709a9446e5f62e8279e81966c38b9
Daniel Alonso <d.alonso@samsung.com>
2021-10-01 21:55:15 -0300

    Remove next, prev buttons, add slider to marcas

--- js/main.js
+++ js/main.js
@@ -3,11 +3,20 @@ $('.slider-principal').slick({
     slidesToScroll: 1,
     autoplay: true,
     autoplaySpeed: 3000,
-    speed: 2500
+    speed: 2500,
+    arrows: false
   });
 
 $('.slider-promos').slick({
     infinite: true,
     slidesToShow: 3,
-    slidesToScroll: 1
+    slidesToScroll: 1,
+    arrows: false
+});
+
+$('.slider-marcas').slick({
+  infinite: false,
+  slidesToShow: 6,
+  slidesToScroll: 1,
+  arrows: false
 });
